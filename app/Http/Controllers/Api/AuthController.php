76d9e1871b7e37a1bfb1bb47febc1df182fa2c69
myatkoheindev <myatkohein.dev@gmail.com>
2023-07-29 15:44:48 +0630

    modify auth and composer.json

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
